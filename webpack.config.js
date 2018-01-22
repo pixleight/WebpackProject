f8161651492d10f0850559ecfc3d623508604a7e
Chris Violette <cvioletterun@gmail.com>
2018-01-22 14:21:58 -0500

    Setting up webpack to build for production

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,10 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
+
   ]
 };
